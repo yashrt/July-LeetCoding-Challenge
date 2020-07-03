d117d3cd98a9882ebda4772ba92b2acfebd8ab7c
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-07-03 22:30:43 +0530

    Day-02 'Binary Tree Level Order Traversal II' solution added

--- Day-02.cpp
+++ Day-02.cpp
@@ -1,1 +1,38 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void dfs(TreeNode* root,int level,vector<vector<int>>& ans){
+        if(!root) 
+            return; 
+        if(level == ans.size()) 
+            ans.push_back({});  
+        
+        ans[level].push_back(root->val); 
+        
+        dfs(root->left,level+1,ans); 
+        dfs(root->right,level+1,ans);
+    }
+    
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if(!root)
+            return {}; 
+        vector<vector<int>>ans;
+        
+        dfs(root,0,ans);
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
