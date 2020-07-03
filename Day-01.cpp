ad160d79629f6bbfcc62c6785f581e7f06767938
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-07-03 22:10:20 +0530

    Day-01 'Arranging Coins' solution added

--- Day-01.cpp
+++ Day-01.cpp
@@ -1,1 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long sum = 0;
+        int i = 0;
+        while(sum<n)
+            sum += ++i;
 
+        return (sum==n)?i:i-1;
+    }
+};
