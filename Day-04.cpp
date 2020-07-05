60d755386a532ff49867ab1c113543559f5a1da6
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-07-05 21:37:35 +0530

    Day-04 'Ugly Number II' solution added

--- Day-04.cpp
+++ Day-04.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int ugly[n];
+        ugly[0] = 1;
+        int count = 1;
+        int idx2 = 0, idx3 = 0, idx5 = 0;
+        while(count < n){
+            int val2 = ugly[idx2]*2;
+            int val3 = ugly[idx3]*3;
+            int val5 = ugly[idx5]*5;
+            int val = min({val2, val3, val5});
+            if(val == val2) 
+              idx2++;
+            if(val == val3) 
+              idx3++;
+            if(val == val5) 
+              idx5++;
+            ugly[count++] = val;
+        }
+        return ugly[n-1];
+    }
+};
