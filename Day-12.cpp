aa7fabd09663e96e661492ac9c665007a2acf5f1
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-07-12 16:58:59 +0530

    Day-12 'Reverse Bits' solution added

---
+++ Day-12.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0, power = 31;
+        while (n != 0)
+        {
+            res += (n & 1) << power;
+            n = n >> 1;
+            power -= 1;
+        }
+        return res;
+    }
+};
