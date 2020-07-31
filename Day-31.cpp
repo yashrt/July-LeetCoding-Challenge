70d4195dbd5017748073343fffcdd227cab0cbb8
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-08-01 00:15:57 +0530

    Day-31 'Climbing Stairs' solution added

--- Day-31.cpp
+++ Day-31.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2)
+            return n;
+    
+        int first = 1,second=2,ans;
+        for(int i=3;i<=n;i++)
+        {
+            ans = first + second;
+            first = second;
+            second = ans;
+        }
+        return ans;
+    }
+};
